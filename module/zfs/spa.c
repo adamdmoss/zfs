cb97462196ddc1cce15f0d046895356e4bba6409
Adam D. Moss <c@yotes.com>
2021-02-12 17:21:37 -0800

    ADAM: low-pri z_wr_iss

--- module/zfs/spa.c
+++ module/zfs/spa.c
@@ -1029,7 +1029,8 @@ spa_taskqs_init(spa_t *spa, zio_type_t t, zio_taskq_type_t q)
 			 */
 			if (t == ZIO_TYPE_WRITE && q == ZIO_TASKQ_ISSUE) {
 #if defined(__linux__)
-				pri++;
+				//pri++;
+				pri = minclsyspri;
 #elif defined(__FreeBSD__)
 				pri += 4;
 #else
