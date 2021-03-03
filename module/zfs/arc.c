387616adbac693f25a3584fb795898d8284aa8c1
Adam Moss <c@yotes.com>
2021-03-03 10:52:45 -0700

    ADAM: ffs, use the right XASSERT3U

--- module/zfs/arc.c
+++ module/zfs/arc.c
@@ -317,8 +317,8 @@ extern	int printk(const char *fmt, ...);
 #else
 #define aprint(...) printf(__VA_ARGS__)
 #endif
-#define XASSERT3(L,C,R) do{const intptr_t lll=(intptr_t)(L); const intptr_t rrr=(intptr_t)(R); if(!(lll C rrr))aprint("ADAM ASSERT FAILURE: %s:%s:%s %s(%lld) %s %s(%lld) failed", __FILE__, __FUNCTION__, __LINE__, #L, (long long int)lll, #C, #R, (long long int)rrr);}while(0)
-#define XASSERT3U XASSERT
+#define XASSERT3(L,C,R) do{const intptr_t lll=(intptr_t)(L); const intptr_t rrr=(intptr_t)(R); if(!(lll C rrr))aprint("ADAM ASSERT FAILURE: %s:%s:%d %s(%lld) %s %s(%lld) failed", __FILE__, __FUNCTION__, __LINE__, #L, (long long int)lll, #C, #R, (long long int)rrr);}while(0)
+#define XASSERT3U XASSERT3
 // ^ bleh, fixme
 
 /* set with ZFS_DEBUG=watch, to enable watchpoints on frozen buffers */
