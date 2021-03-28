b9d983fd354f26d196a1bb570c7d6377779d8c18
Adam D. Moss <c@yotes.com>
2021-03-27 18:30:45 -0700

    more conflict fix

--- include/os/linux/kernel/linux/kmap_compat.h
+++ include/os/linux/kernel/linux/kmap_compat.h
@@ -31,6 +31,7 @@
 
 /* 2.6.37 API change */
 #define	zfs_kmap_atomic(page)	kmap_atomic(page)
+#define        zfs_kunmap_atomic(addr) kunmap_atomic(addr)
 #define	zfs_kmap(page)				kmap(page)
 #define	zfs_kunmap(page)			kunmap(page)
 
