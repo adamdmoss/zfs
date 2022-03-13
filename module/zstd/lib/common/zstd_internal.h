2dc536d64deaa6a767a05967379df160c82301d7
Adam D. Moss <c@yotes.com>
2022-03-12 16:15:05 -0800

    Revert "Re-apply 6ba2e72b, silence lint"
    This reverts commit fe2ea67ddd6359253a772c68e4bebafbdd2505da.

--- module/zstd/lib/common/zstd_internal.h
+++ module/zstd/lib/common/zstd_internal.h
@@ -19,7 +19,7 @@
 /*-*************************************
 *  Dependencies
 ***************************************/
-#if !defined(ZSTD_NO_INTRINSICS) && defined(__ARM_NEON)
+#ifdef __aarch64__
 #include <arm_neon.h>
 #endif
 #include "compiler.h"
@@ -227,7 +227,7 @@ static const U32 OF_defaultNormLog = OF_DEFAULTNORMLOG;
 *  Shared functions to include for inlining
 *********************************************/
 static void ZSTD_copy8(void* dst, const void* src) {
-#if !defined(ZSTD_NO_INTRINSICS) && defined(__ARM_NEON)
+#ifdef __aarch64__
     vst1_u8((uint8_t*)dst, vld1_u8((const uint8_t*)src));
 #else
     memcpy(dst, src, 8);
@@ -236,7 +236,7 @@ static void ZSTD_copy8(void* dst, const void* src) {
 
 #define COPY8(d,s) { ZSTD_copy8(d,s); d+=8; s+=8; }
 static void ZSTD_copy16(void* dst, const void* src) {
-#if !defined(ZSTD_NO_INTRINSICS) && defined(__ARM_NEON)
+#ifdef __aarch64__
     vst1q_u8((uint8_t*)dst, vld1q_u8((const uint8_t*)src));
 #else
     memcpy(dst, src, 16);
